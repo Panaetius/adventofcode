4b59ccfc507ced6848a522945b8914fb7f738304
Ralf Grubenmann <ralf.grubenmann@gmail.com>
2023-12-20 09:04:36 +0100

    day 19 part 2

--- parts_19/src/main.rs
+++ parts_19/src/main.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
+use std::cmp::{max, min};
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::prelude::*;
@@ -29,6 +31,37 @@ impl<'a> Ruleset<'a> {
         }
         self.end
     }
+    fn range<'b>(
+        &'b self,
+        rangeset: HashMap<&'b str, Range>,
+    ) -> (Vec<(&str, HashMap<&str, Range>)>, Vec<HashMap<&str, Range>>) {
+        let mut follow = Vec::new();
+        let mut acc = Vec::new();
+        let mut rangeset = rangeset;
+        for rule in self.rules.clone() {
+            let (a, r) = rule.disc.split(rangeset.get(rule.label).unwrap().clone());
+            if let Some(r) = a {
+                let mut n = rangeset.clone();
+                n.entry(rule.label).and_modify(|v| *v = r);
+                match rule.target {
+                    Action::Accepted => acc.push(n.clone()),
+                    Action::Rejected => {}
+                    Action::Forward(s) => follow.push((s, n.clone())),
+                }
+            }
+            if let Some(r) = r {
+                let mut n = rangeset.clone();
+                n.entry(rule.label).and_modify(|v| *v = r);
+                rangeset = n;
+            }
+        }
+        match self.end {
+            Action::Accepted => acc.push(rangeset.clone()),
+            Action::Rejected => {}
+            Action::Forward(s) => follow.push((s, rangeset.clone())),
+        }
+        (follow, acc)
+    }
 }
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone)]
 enum Discriminator {
@@ -40,6 +73,46 @@ impl Discriminator {
         match self {
             Discriminator::LT(v) => value < *v,
             Discriminator::GT(v) => value > *v,
+        }
+    }
+    fn split(&self, range: Range) -> (Option<Range>, Option<Range>) {
+        match self {
+            Discriminator::LT(v) => {
+                if *v <= range.start {
+                    (None, Some(range))
+                } else if range.end < *v {
+                    (Some(range), None)
+                } else {
+                    (
+                        Some(Range {
+                            start: range.start,
+                            end: v - 1,
+                        }),
+                        Some(Range {
+                            start: *v,
+                            end: range.end,
+                        }),
+                    )
+                }
+            }
+            Discriminator::GT(v) => {
+                if *v < range.start {
+                    (Some(range), None)
+                } else if range.end <= *v {
+                    (None, Some(range))
+                } else {
+                    (
+                        Some(Range {
+                            start: v + 1,
+                            end: range.end,
+                        }),
+                        Some(Range {
+                            start: range.start,
+                            end: *v,
+                        }),
+                    )
+                }
+            }
         }
     }
 }
@@ -56,6 +129,29 @@ impl<'a> Rule<'a> {
         } else {
             None
         }
+    }
+}
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone)]
+struct Range {
+    start: usize,
+    end: usize,
+}
+impl Range {
+    fn intersect(&self, other: Range) -> Option<Range> {
+        if self.end < other.start || self.start > other.end {
+            None
+        } else {
+            Some(Range {
+                start: max(self.start, other.start),
+                end: min(self.end, other.end),
+            })
+        }
+    }
+    fn size(&self) -> usize {
+        self.end - self.start + 1
+    }
+    fn add(&self, other: Range) -> usize {
+        self.size() + other.size() - self.intersect(other).map(|i| i.size()).unwrap_or(0)
     }
 }
 fn rules(i: &str) -> IResult<&str, HashMap<&str, Ruleset>> {
@@ -143,26 +239,88 @@ fn main() -> std::io::Result<()> {
     let (i, (rules, parts)) = separated_pair(rules, newline, parts)(&contents).unwrap();
     println!("{:?}", rules);
     println!("{:?}", parts);
-    let mut accepted: Vec<HashMap<&str, usize>> = Vec::new();
-    for part in parts {
-        let mut curr = "in";
-        while true {
-            match rules[curr].process(&part) {
-                Action::Accepted => {
-                    accepted.push(part);
-                    break;
-                }
-                Action::Rejected => {
-                    break;
-                }
-                Action::Forward(s) => {
-                    curr = s;
-                }
-            }
-        }
+    // let mut accepted: Vec<HashMap<&str, usize>> = Vec::new();
+    // for part in parts {
+    //     let mut curr = "in";
+    //     while true {
+    //         match rules[curr].process(&part) {
+    //             Action::Accepted => {
+    //                 accepted.push(part);
+    //                 break;
+    //             }
+    //             Action::Rejected => {
+    //                 break;
+    //             }
+    //             Action::Forward(s) => {
+    //                 curr = s;
+    //             }
+    //         }
+    //     }
+    // }
+    // let result: usize = accepted.iter().map(|p| p.values().sum::<usize>()).sum();
+    // println!("{:?}", result);
+    let mut accepted: Vec<HashMap<&str, Range>> = Vec::new();
+    let mut stack = vec![(
+        "in",
+        HashMap::from([
+            (
+                "x",
+                Range {
+                    start: 1,
+                    end: 4000,
+                },
+            ),
+            (
+                "m",
+                Range {
+                    start: 1,
+                    end: 4000,
+                },
+            ),
+            (
+                "a",
+                Range {
+                    start: 1,
+                    end: 4000,
+                },
+            ),
+            (
+                "s",
+                Range {
+                    start: 1,
+                    end: 4000,
+                },
+            ),
+        ]),
+    )];
+    while stack.len() > 0 {
+        let cur = stack.pop().unwrap();
+        let rule = rules.get(&cur.0).unwrap();
+        let (follow, acc) = rule.range(cur.1);
+        accepted.extend(acc);
+        stack.extend(follow);
     }
-    let result: usize = accepted.iter().map(|p| p.values().sum::<usize>()).sum();
-    println!("{:?}", result);
+    println!("{:?}", accepted);
+    let mut sum: usize = accepted
+        .iter()
+        .map(|v| v.values().map(|i| i.size()).fold(1, |a, c| a * c))
+        .sum();
+    sum -= accepted
+        .iter()
+        .combinations(2)
+        .map(|v| {
+            let a = v[0];
+            let b = v[1];
+            a.iter()
+                .map(|(k, v)| {
+                    v.intersect(b[k].clone())
+                        .and_then(|i| Some(i.size()))
+                        .unwrap_or(0)
+                })
+                .fold(1, |a, c| a * c)
+        })
+        .sum::<usize>();
+    println!("{}", sum);
 
     Ok(())
 }
